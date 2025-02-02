1ec5144800edee9f31d062453e4cc545c1a82752
sherif-elmetainy <sherif.elmetainy@gmail.com>
2025-02-02 20:48:43 +0200

    add angular-globalite library project

---
+++ projects/angular-globalite/src/lib/angular-globalite.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'lib-angular-globalite',
+	imports: [],
+	template: ` <p>angular-globalite works!</p> `,
+	styles: ``,
+})
+export class AngularGlobaliteComponent {}
