134b96f36bd70709cef4674bcf4ff7ea3c57450f
sherif-elmetainy <sherif.elmetainy@gmail.com>
2025-02-02 20:47:34 +0200

    add angular bootstrap library project

---
+++ projects/angular-bootstrap/src/lib/angular-bootstrap.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'lib-angular-bootstrap',
+	imports: [],
+	template: ` <p>angular-bootstrap works!</p> `,
+	styles: ``,
+})
+export class AngularBootstrapComponent {}
